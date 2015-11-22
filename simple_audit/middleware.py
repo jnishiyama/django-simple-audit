e3eb9f8539695e6da388fc6fa898c866e7af98ee
James Finucane <jnishiyama@users.noreply.github.com>
2015-11-22 14:12:36 +0200

    Update middleware.py

--- simple_audit/middleware.py
+++ simple_audit/middleware.py
@@ -41,7 +41,7 @@ class TrackingRequestOnThreadLocalMiddleware(object):
                 authenticator = self._import_from_string(settings.DJANGO_SIMPLE_AUDIT_REST_FRAMEWORK_AUTHENTICATOR, 'DJANGO_SIMPLE_AUDIT_AUTHENTICATOR')
                 user_auth_tuple = authenticator().authenticate(request)
                 if user_auth_tuple is not None:
-                    request.user = user_auth_tuple[0]
+                    request.user, request.auth = user_auth_tuple
                     ip = self._get_ip(request)
                     AuditRequest.new_request(request.get_full_path(), request.user, ip)
 
